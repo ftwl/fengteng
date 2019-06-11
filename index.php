f107c70a643778f91561e853eddee55881009dbc
Jokky <13801489449@163.com>
2019-06-11 21:41:42 +0800

    add images

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
         <link rel="stylesheet" href="templates/default/style/shards-extras.min.css">
         <link rel="stylesheet" href="templates/default/style/about.css">
         <link rel="stylesheet" href="templates/default/style/parterner.css">
+        <link rel="stylesheet" href="templates/default/style/swiper.min.css">
+        <script src="templates/default/js/swiper.min.js"/>
         <script src="templates/default/js/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
         <script src="templates/default/js/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
         <script src="templates/default/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
@@ -84,7 +86,24 @@
         <div class="container-fluid">
           <div class="row">
             <div class="app-screenshot col-lg-4 col-md-12 col-sm-12 px-0 py-5">
-              <img class="mt-auto mb-auto" src="templates/default/images/iphone-vip.jpg" alt="App Screenshot - Shards App Promo Demo Page">
+              <div class="swiper-container">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <img src="templates/default/images/iphone-vip.jpg"/>
+                  </div>
+                  <div class="swiper-slide">
+                    <img src="templates/default/images/iphone-vip.jpg"/>
+                  </div>
+                  <div class="swiper-slide">
+                    <img src="templates/default/images/iphone-vip.jpg"/>
+                  </div>
+                  <div class="swiper-slide">
+                    <img src="templates/default/images/iphone-vip.jpg"/>
+                  </div>
+                </div>
+
+                <div class="swiper-pagination"></div>
+              </div>
             </div>
 
             <!-- App Features Wrapper -->
@@ -314,5 +333,13 @@
       </footer>
       <!-- / Footer Section -->
       <!-- JavaScript Dependencies -->
+
+      <script>
+        var swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+      </script>
     </body>
    </html>
