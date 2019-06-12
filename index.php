7ade8c05ea2011efe6711c3d302efd5b60ccac97
Jokky <13801489449@163.com>
2019-06-12 14:06:21 +0800

    2019.06.12

--- index.php
+++ index.php
@@ -185,52 +185,6 @@
       </div> -->
       <!-- / Testimonials Section -->
 
-      <!-- Our Blog Section -->
-      <!-- <div class="blog section section-invert py-4">
-        <h3 class="section-title text-center m-5">Latest Posts</h3>
-        <div class="container">
-          <div class="py-4">
-            <div class="row">
-              <div class="card-deck">
-                <div class="col-md-12 col-lg-4">
-                  <div class="card mb-4">
-                    <img class="card-img-top" src="templates/default/images/common/card-1.jpg" alt="Card image cap">
-                    <div class="card-body">
-                      <h4 class="card-title">Find Great Places to Work While Travelling</h4>
-                      <p class="card-text">He seems sinking under the evidence could not only grieve and a visit. The father is to bless and placed in his length hid...</p>
-                      <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-md-12 col-lg-4">
-                  <div class="card mb-4">
-                    <img class="card-img-top" src="templates/default/images/common/card-3.jpg" alt="Card image cap">
-                    <div class="card-body">
-                      <h4 class="card-title">Quick Tips for Improving Your Website's Design</h4>
-                      <p class="card-text">He seems sinking under the evidence could not only grieve and a visit. The father is to bless and placed in his length hid...</p>
-                      <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                    </div>
-                  </div>
-                </div>
-
-                <div class="col-md-12 col-lg-4">
-                  <div class="card mb-4">
-                    <img class="card-img-top" src="templates/default/images/common/card-2.jpg" alt="Card image cap">
-                    <div class="card-body">
-                      <h4 class="card-title">A Designer's Tips While Travelling and Working</h4>
-                      <p class="card-text">He seems sinking under the evidence could not only grieve and a visit. The father is to bless and placed in his length hid...</p>
-                      <a class="btn btn-outline-success btn-pill" href="#">Read More &rarr;</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> -->
-      <!-- / Our Blog Section -->
-
       <!-- Subscribe Section -->
       <div class="subscribe section bg-dark py-4 text">
         <div class="wp">
